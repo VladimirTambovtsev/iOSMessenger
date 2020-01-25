b61871744516daa1c6978b2e501b1aa7979686bc
VladimirTambovtsev <tambovcev99@mail.ru>
2020-01-25 20:20:43 +0300

    Initial Commit

---
+++ Slack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Slack
+//
+//  Created by Vladimir on 25.01.2020.
+//  Copyright © 2020 Vladimir Tambovtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
